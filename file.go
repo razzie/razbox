9b6b5a536cd05e42e17c70b62be41af078d9cd5e
Gábor Görzsöny <gabor@gorzsony.com>
2020-05-14 23:44:00 +0200

    Improved large file serving #2

--- file.go
+++ file.go
@@ -9,6 +9,8 @@ import (
 	"os"
 	"path"
 	"path/filepath"
+	"strconv"
+	"strings"
 	"time"
 )
 
@@ -124,6 +126,111 @@ func (f *File) HasTag(tag string) bool {
 	return false
 }
 
+// credit: https://github.com/rb-de0/go-mp4-stream/
 func (f *File) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, path.Join(Root, f.RelPath+".bin"))
+	const BUFSIZE = 1024 * 8
+
+	file, err := os.Open(path.Join(Root, f.RelPath+".bin"))
+	if err != nil {
+		w.WriteHeader(500)
+		return
+	}
+	defer file.Close()
+
+	fi, err := file.Stat()
+	if err != nil {
+		w.WriteHeader(500)
+		return
+	}
+
+	fileSize := int(fi.Size())
+
+	if len(r.Header.Get("Range")) == 0 {
+		contentLength := strconv.Itoa(fileSize)
+		contentEnd := strconv.Itoa(fileSize - 1)
+
+		w.Header().Set("Content-Type", f.MIME)
+		w.Header().Set("Accept-Ranges", "bytes")
+		w.Header().Set("Content-Length", contentLength)
+		w.Header().Set("Content-Range", "bytes 0-"+contentEnd+"/"+contentLength)
+		w.WriteHeader(200)
+
+		buffer := make([]byte, BUFSIZE)
+
+		for {
+			n, err := file.Read(buffer)
+			if n == 0 {
+				break
+			}
+			if err != nil {
+				break
+			}
+
+			data := buffer[:n]
+			w.Write(data)
+			w.(http.Flusher).Flush()
+		}
+	} else {
+		rangeParam := strings.Split(r.Header.Get("Range"), "=")[1]
+		splitParams := strings.Split(rangeParam, "-")
+
+		// response values
+		contentStartValue := 0
+		contentStart := strconv.Itoa(contentStartValue)
+		contentEndValue := fileSize - 1
+		contentEnd := strconv.Itoa(contentEndValue)
+		contentSize := strconv.Itoa(fileSize)
+
+		if len(splitParams) > 0 {
+			contentStartValue, err = strconv.Atoi(splitParams[0])
+			if err != nil {
+				contentStartValue = 0
+			}
+			contentStart = strconv.Itoa(contentStartValue)
+		}
+
+		if len(splitParams) > 1 {
+			contentEndValue, err = strconv.Atoi(splitParams[1])
+			if err != nil {
+				contentEndValue = fileSize - 1
+			}
+			contentEnd = strconv.Itoa(contentEndValue)
+		}
+
+		contentLength := strconv.Itoa(contentEndValue - contentStartValue + 1)
+
+		w.Header().Set("Content-Type", f.MIME)
+		w.Header().Set("Accept-Ranges", "bytes")
+		w.Header().Set("Content-Length", contentLength)
+		w.Header().Set("Content-Range", "bytes "+contentStart+"-"+contentEnd+"/"+contentSize)
+		w.WriteHeader(206)
+
+		buffer := make([]byte, BUFSIZE)
+
+		file.Seek(int64(contentStartValue), 0)
+
+		writeBytes := 0
+
+		for {
+			n, err := file.Read(buffer)
+			writeBytes += n
+			if n == 0 {
+				break
+			}
+			if err != nil {
+				break
+			}
+
+			if writeBytes >= contentEndValue {
+				data := buffer[:BUFSIZE-writeBytes+contentEndValue+1]
+				w.Write(data)
+				w.(http.Flusher).Flush()
+				break
+			}
+
+			data := buffer[:n]
+			w.Write(data)
+			w.(http.Flusher).Flush()
+		}
+	}
 }
