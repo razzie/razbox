cffc9c5b80b61d7b956f9b95a135a599acf0e21b
Gábor Görzsöny <gabor@gorzsony.com>
2021-01-15 00:56:55 +0100

    More accurate elapsed times

--- vendor/github.com/razzie/beepboop/funcs.go
+++ vendor/github.com/razzie/beepboop/funcs.go
@@ -3,90 +3,56 @@ package beepboop
 import (
 	"fmt"
 	"html/template"
-	"math"
-	"strconv"
-	"strings"
 	"time"
 )
 
 // TemplateFuncs is beepboop's built in template FuncMap
 var TemplateFuncs = template.FuncMap{
 	"TimeElapsed": func(then int64) string {
-		return TimeElapsed(time.Now(), time.Unix(then, 0), false)
+		return TimeElapsed(time.Now(), time.Unix(then, 0))
 	},
 	"ByteCountSI":  ByteCountSI,
 	"ByteCountIEC": ByteCountIEC,
 }
 
-func s(x float64) string {
-	if int(x) == 1 {
-		return ""
-	}
-	return "s"
-}
-
 // TimeElapsed returns the elapsed time in human readable format (such as "5 days ago")
-func TimeElapsed(now time.Time, then time.Time, full bool) string {
-	var parts []string
-	var text string
-
-	year2, month2, day2 := now.Date()
-	hour2, minute2, second2 := now.Clock()
-
-	year1, month1, day1 := then.Date()
-	hour1, minute1, second1 := then.Clock()
-
-	year := math.Abs(float64(int(year2 - year1)))
-	month := math.Abs(float64(int(month2 - month1)))
-	day := math.Abs(float64(int(day2 - day1)))
-	hour := math.Abs(float64(int(hour2 - hour1)))
-	minute := math.Abs(float64(int(minute2 - minute1)))
-	second := math.Abs(float64(int(second2 - second1)))
-
-	week := math.Floor(day / 7)
-
-	if year > 0 {
-		parts = append(parts, strconv.Itoa(int(year))+" year"+s(year))
+func TimeElapsed(now time.Time, then time.Time) string {
+	text := func(unit string, amount int64) string {
+		if amount > 1 {
+			unit += "s"
+		}
+		if now.After(then) {
+			return fmt.Sprintf("%d %s ago", amount, unit)
+		}
+		return fmt.Sprintf("%d %s after", amount, unit)
 	}
 
-	if month > 0 {
-		parts = append(parts, strconv.Itoa(int(month))+" month"+s(month))
-	}
+	seconds := now.Unix() - then.Unix()
+	minutes := seconds / 60
+	hours := minutes / 60
+	days := hours / 24
+	weeks := days / 7
+	months := days / 30
+	years := days / 365
 
-	if week > 0 {
-		parts = append(parts, strconv.Itoa(int(week))+" week"+s(week))
-	}
-
-	if day > 0 {
-		parts = append(parts, strconv.Itoa(int(day))+" day"+s(day))
-	}
-
-	if hour > 0 {
-		parts = append(parts, strconv.Itoa(int(hour))+" hour"+s(hour))
-	}
-
-	if minute > 0 {
-		parts = append(parts, strconv.Itoa(int(minute))+" minute"+s(minute))
-	}
-
-	if second > 0 {
-		parts = append(parts, strconv.Itoa(int(second))+" second"+s(second))
-	}
-
-	if now.After(then) {
-		text = " ago"
-	} else {
-		text = " after"
-	}
-
-	if len(parts) == 0 {
+	switch {
+	case years > 0:
+		return text("year", years)
+	case months > 0:
+		return text("month", months)
+	case weeks > 0:
+		return text("week", weeks)
+	case days > 0:
+		return text("day", days)
+	case hours > 0:
+		return text("hour", hours)
+	case minutes > 0:
+		return text("minute", minutes)
+	case seconds > 10:
+		return text("second", seconds)
+	default:
 		return "just now"
 	}
-
-	if full {
-		return strings.Join(parts, ", ") + text
-	}
-	return parts[0] + text
 }
 
 // ByteCountSI ...
